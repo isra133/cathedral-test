2c0a9fc174c7ca06fe1486127cfc6505e1fb1ba2
Israel Palomar <israel.palomar@doubledot.es>
2022-01-14 11:26:48 +0100

    Unnecessary items removed

--- src/App.vue
+++ src/App.vue
@@ -22,9 +22,6 @@
 @import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&family=VT323&display=swap');
 @import url('https://unpkg.com/nes.css@latest/css/nes.min.css');
 
-
-
-@import '@/assets/vars.scss';
 @import '@/assets/style.scss';
 
 </style>
--- src/main.js
+++ src/main.js
@@ -6,18 +6,5 @@ import router from './router'
 
 import store from './store'
 
-/*Validator Base*/
-// import { ValidationProvider } from 'vee-validate';
-// import { ValidationObserver } from 'vee-validate';
-// Vue.component('ValidationProvider', ValidationProvider);
-// Vue.component('ValidationObserver', ValidationObserver);
-// import { extend } from 'vee-validate';
-// import {required } from 'vee-validate/dist/rules';
-
-// extend('required', {
-//   ...required,
-//   message: 'Debes responder para continuar'
-// });
-
 
 createApp(App).use(store).use(router).mount('#app')
